c9be41d2fea0fda0d1a5339345a5b191f967f459
giriHazto1999 <33338986+giriHazto1999@users.noreply.github.com>
2017-11-13 15:33:48 +0530

    Add files via upload

--- selection and insertion sort.py
+++ selection and insertion sort.py
@@ -1,15 +1,14 @@
-def insertsort(L):
+def selectsort(L): 
+    for slot in range(len(L)-1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, slot + 1):
+            if L[location] > L[positionOfMax]:
+                positionOfMax = location
 
-    for index in range(1, len(L)):
-        key = L[index]
-        j = index - 1
+        temp             = L[slot]
+        L[slot]          = L[positionOfMax]
+        L[positionOfMax] = temp
+    return L
 
-        while j >= 0 and (L[j] > key):
-           L[j + 1] = L[j]
-           j = j - 1
-           L[j + 1] = key
-          
-    return L 
-
-L = [5, 4, 0, 2, 3]
-print(insertsort(L))
+L = [14, 24, 25, 26, 21]
+print(selectsort(L))
