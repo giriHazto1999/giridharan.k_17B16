790cafc1e558896cc85673b1d40d85738db20534
giriHazto1999 <33338986+giriHazto1999@users.noreply.github.com>
2017-11-13 16:17:23 +0530

    Add files via upload

--- binary serach.py
+++ binary serach.py
@@ -1,19 +1,19 @@
-def binary_search(my1, token):
+def binary_search(myl, token):
     found = False
-
     left = 0
-    right = len(my1)-1
-
+    right = len(myl)-1
     while left <= right and not found:
         mid = (right+left)//2
-        if my1[mid] == token:
+        if myl[mid] == token:
             found = True
 
-        if my1[mid] > token:
+        if myl[mid] > token:
             right = mid - 1
         else:
             left = mid + 1
     return found
 
-found = [45]
-print(binary_search(found))
+myl = [5,4.3,0,29,4.2]
+token = 0
+
+print(binary_search(myl, token))
